78f1eabeb06f8c5c1ebb2c68f5b8e698720adff8
Nikola Zivkovic <nzivkovic7@gmail.com>
2022-12-10 22:56:49 +0100

    Added sendSMS function, added validation for city picker text field, refactored zone buttons pressed IBActions

--- UkucajSe/ViewController.swift
+++ UkucajSe/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var plateNumberInput: UITextField!
     @IBOutlet weak var cityPickerField: UITextField!
+    @IBOutlet weak var zone1Button: UIButton!
+    @IBOutlet weak var zone2Button: UIButton!
+    @IBOutlet weak var zone3Button: UIButton!
     var plateNumber = ""
     let cities = [
         gradNis,
@@ -41,41 +44,73 @@ class ViewController: UIViewController {
         plateNumberInput.resignFirstResponder()
     }
     
-    @IBAction func Zona1ButtonPressed(_ sender: UIButton) {
+    func sendSMS(with plateNum: String, to number: Int) {
+        let sms = "sms:\(number)&body=\(plateNum.uppercased())"
+        let strURL = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        UIApplication.shared.open(URL(string: strURL)!, options: [:], completionHandler: nil)
+    }
+    
+//    func pickerWarning() {
+//        cityPickerField.backgroundColor = .red
+//        cityPickerField.text = "Izaberite grad"
+//    }
+    
+    @IBAction func cityIsChosen(_ sender: UITextField) {
+        if cityPickerField.text == "" {
+            cityPickerField.backgroundColor = .red
+            cityPickerField.text = "Izaberite grad"
+            zone1Button.isEnabled = false
+            zone2Button.isEnabled = false
+            zone3Button.isEnabled = false
+        } else {
+            cityPickerField.backgroundColor = .clear
+            zone1Button.isEnabled = true
+            zone2Button.isEnabled = true
+            zone3Button.isEnabled = true
+        }
+    }
+    
+    @IBAction func zone1ButtonPressed(_ sender: UIButton) {
         switch cityPickerField.text{
         case gradNis.name:
-            let sms = "sms:\(gradNis.zone1.number)&body=\(plateNumber.uppercased())"
-            let strURL = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-            UIApplication.shared.open(URL(string: strURL)!, options: [:], completionHandler: nil)
+            sendSMS(with: plateNumber, to: gradNis.zone1.number)
         case gradBeograd.name:
-            let sms = "sms:\(gradBeograd.zone1.number)&body=\(plateNumber.uppercased())"
-            let strURL = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-            UIApplication.shared.open(URL(string: strURL)!, options: [:], completionHandler: nil)
+            sendSMS(with: plateNumber, to: gradBeograd.zone1.number)
         case gradNoviSad.name:
-            let sms = "sms:\(gradNoviSad.zone1.number)&body=\(plateNumber.uppercased())"
-            let strURL = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-            UIApplication.shared.open(URL(string: strURL)!, options: [:], completionHandler: nil)
+            sendSMS(with: plateNumber, to: gradNoviSad.zone1.number)
         default:
             cityPickerField.text = "Izaberite grad"
-            cityPickerField.textColor = .white
-            cityPickerField.backgroundColor = .red
         }
         print(plateNumber)
     }
     
-//    @IBAction func zona2ButtonPressed(_ sender: UIButton) {
-//        let sms = "sms:\(gradNis.zone2.number)&body=\(plateNumber.uppercased())"
-//        let strURL = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-//        UIApplication.shared.open(URL(string: strURL)!, options: [:], completionHandler: nil)
-//        print(plateNumber)
-//    }
-//
-//    @IBAction func zona3ButtonePressed(_ sender: UIButton) {
-//        let sms = "sms:\(gradNis.zone3.number)&body=\(plateNumber.uppercased())"
-//        let strURL = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-//        UIApplication.shared.open(URL(string: strURL)!, options: [:], completionHandler: nil)
-//        print(plateNumber)
-//    }
+    @IBAction func zone2ButtonPressed(_ sender: UIButton) {
+        switch cityPickerField.text{
+        case gradNis.name:
+            sendSMS(with: plateNumber, to: gradNis.zone2.number)
+        case gradBeograd.name:
+            sendSMS(with: plateNumber, to: gradBeograd.zone2.number)
+        case gradNoviSad.name:
+            sendSMS(with: plateNumber, to: gradNoviSad.zone2.number)
+        default:
+            cityPickerField.text = "Izaberite grad"
+        }
+        print(plateNumber)
+    }
+
+    @IBAction func zone3ButtonPressed(_ sender: UIButton) {
+        switch cityPickerField.text{
+        case gradNis.name:
+            sendSMS(with: plateNumber, to: gradNis.zone3.number)
+        case gradBeograd.name:
+            sendSMS(with: plateNumber, to: gradBeograd.zone3.number)
+        case gradNoviSad.name:
+            sendSMS(with: plateNumber, to: gradNoviSad.zone3.number)
+        default:
+            cityPickerField.text = "Izaberite grad"
+        }
+        print(plateNumber)
+    }
     
     @IBAction func doneButtonPressed(_ sender: UIButton) {
         plateNumber = String(plateNumberInput.text!)
